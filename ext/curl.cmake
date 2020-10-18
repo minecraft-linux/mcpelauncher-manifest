08696c084fbea26b023d66fde78de95e684e2272
Christopher Homberger <christopher.homberger@web.de>
2020-10-18 22:08:23 +0200

    Update curl version, improved openssl handling

--- ext/curl.cmake
+++ ext/curl.cmake
@@ -1,17 +1,20 @@
 include(ExternalProject)
 
 if (NOT APPLE)
-    find_package(OpenSSL REQUIRED)
+    find_package(OpenSSL REQUIRED COMPONENTS SSL Crypto)
 endif()
 
-set(CURL_EXT_EXTRA_OPTIONS )
+if (NOT CURL_EXT_EXTRA_OPTIONS)
+    set(CURL_EXT_EXTRA_OPTIONS )
+endif()
+
 if (DEFINED OPENSSL_ROOT_DIR)
     list(APPEND CURL_EXT_EXTRA_OPTIONS "-DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}")
 endif()
 
 ExternalProject_Add(
         curl_ext
-        URL "http://curl.haxx.se/download/curl-7.61.0.tar.gz"
+        URL "http://curl.haxx.se/download/curl-7.72.0.tar.gz"
         INSTALL_DIR ${CMAKE_BINARY_DIR}/ext/curl
         CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/ext/curl" "-DBUILD_CURL_EXE=OFF" "-DBUILD_SHARED_LIBS=OFF" "-DCURL_STATICLIB=ON" "-DCURL_DISABLE_LDAP=ON" "-DCMAKE_USE_LIBSSH2=OFF" "-DCMAKE_USE_OPENLDAP=OFF" "-DCMAKE_C_FLAGS=-m32" "-DCMAKE_LINK_FLAGS=-m32" "-DCMAKE_LIBRARY_ARCHITECTURE=${CMAKE_LIBRARY_ARCHITECTURE}" ${CURL_EXT_EXTRA_OPTIONS}
 )
@@ -21,7 +24,7 @@ add_dependencies(curl curl_ext)
 set_property(TARGET curl PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/ext/curl/lib/libcurl.a)
 set_property(TARGET curl PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_BINARY_DIR}/ext/curl/include/)
 if (NOT APPLE)
-    set_property(TARGET curl PROPERTY INTERFACE_LINK_LIBRARIES ${OPENSSL_LIBRARIES})
+    set_property(TARGET curl PROPERTY INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
 endif()
 
 set(CURL_FOUND TRUE)
