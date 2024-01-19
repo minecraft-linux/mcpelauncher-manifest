ca10280a053006dda1279b00010fa48d5b5721d8
Christopher Homberger <christopher.homberger@web.de>
2024-01-19 20:55:01 +0100

    add sdl3

---
+++ ext/sdl3.cmake
@@ -0,0 +1,11 @@
+# Create an option to switch between a system sdl library and a vendored SDL library
+option(SDL3_VENDORED "Use vendored libraries" OFF)
+
+if(SDL3_VENDORED)
+    add_subdirectory(sdl3 EXCLUDE_FROM_ALL)
+else()
+    # 1. Look for a SDL3 package,
+    # 2. look for the SDL3-shared component, and
+    # 3. fail if the shared component cannot be found.
+    find_package(SDL3 REQUIRED CONFIG REQUIRED COMPONENTS SDL3-shared)
+endif()
